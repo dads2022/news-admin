cc918e886e1f5adab78a6d8274d61618a8aabdd3
zunohan <pnlan1406@gmail.com>
2022-03-24 23:37:00 +0700

    pc edit

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -15,7 +15,7 @@ export default function Layout() {
     return (
         <div className="flex h-screen overflow-hidden">
             <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-            <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+            <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden dark:bg-slate-900 dark:text-slate-100">
                 <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
                 <main className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
                     <div className="sm:flex sm:justify-between sm:items-center mb-8">
